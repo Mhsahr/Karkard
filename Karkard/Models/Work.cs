5ba456c6d6b765a72e9764c906c3c7c763f71523
Mahsa <m.hamedd1380@gmail.com>
2023-09-06 18:19:07 +0330

    Initial commit

---
+++ Karkard/Models/Work.cs
@@ -0,0 +1,10 @@
+﻿namespace Karkard.Models
+{
+    public class Work
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string From { get; set; } 
+        public string Until { get; set; }
+    }
+}
